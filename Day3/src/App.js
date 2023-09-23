8f52c9193d669d9331142e131b84bdbb776e1697
Bhuvanx07 <145004441+Bhuvanx07@users.noreply.github.com>
2023-09-23 12:19:00 +0530

    Add files via upload

---
+++ Day3/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App';
+import { Login } from './pages/Login';
+// import './App.css';
+import { Register } from './pages/Register';
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={< Login />} />
+          <Route path='/Login' index element={< Login />} />
+          <Route path='/Register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+export default App;
