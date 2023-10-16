51c40095bb37a15b680d517b290e076dbbffd2ed
Bhuvanx07 <145004441+Bhuvanx07@users.noreply.github.com>
2023-10-16 12:48:44 +0530

    Add files via upload

---
+++ day 11 backend/src/main/java/com/iamneo/security/service/AuthenticationService.java
@@ -0,0 +1,58 @@
+package com.iamneo.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.iamneo.security.dto.request.AuthenticationRequest;
+import com.iamneo.security.dto.request.RegisterRequest;
+import com.iamneo.security.dto.response.AuthenticationResponse;
+import com.iamneo.security.entity.Role;
+import com.iamneo.security.entity.User;
+import com.iamneo.security.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+	@Autowired
+    private  UserRepository userRepository;
+	
+    private  final PasswordEncoder passwordEncoder;
+    private  final JwtService jwtService;
+    private  final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        User user = User
+        		.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .contact(request.getContact())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+    	authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+}
